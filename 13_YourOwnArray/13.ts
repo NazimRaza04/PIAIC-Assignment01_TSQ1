ab2569562e7d40a3e8c20e07f6afe45a729498c8
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:20:58 +0500

    Add files via upload

---
+++ 13_YourOwnArray/13.ts
@@ -0,0 +1,12 @@
+// 13. Your Own Array: Think of your favorite mode of transportation, 
+// such as a motorcycle or a car, and make a list that stores several examples. 
+// Use your list to print a series of statements about these items, such as
+//  “I would like to own a Honda motorcycle.”
+
+//13. Your Own Array:
+
+let list = ["Tesla", "Maybach", "RolceRoyce"];
+
+console.log (`${list[0]} is a well known brand for EV.`);
+console.log (`InshaAllah! I'll own the latest ${list[1]}, soon.`);
+console.log (`I wish to have a ${list[2]} in my collection.`);
