d03604627c6ba64d5d2b6bac0dc53aae8b996a53
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:20:02 +0500

    Add files via upload

---
+++ 12_Greetings/12.js
@@ -0,0 +1,7 @@
+"use strict";
+// 12. Greetings: Start with the array you used in Exercise 11, 
+// but instead of just printing each person’s name, print a message to them. 
+// The text of each message should be the same, 
+// but each message should be personalized with the person’s name.
+const names = ["Raheel", "Amjad", "Zubair", "Nomeel"];
+names.forEach((item, index) => console.log(`${item}, How are you doing today?`));
