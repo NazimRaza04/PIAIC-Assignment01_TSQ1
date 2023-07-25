068db8f1daf842ddf6ba466bedd4111716c1d531
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:31:19 +0500

    Add files via upload

---
+++ 20_ArrayList/20.ts
@@ -0,0 +1,15 @@
+// 20. Think of something you could store in a array. For example, 
+// you could make a list of mountains, rivers, countries, cities, languages, 
+// or anything else you’d like. Write a program that creates a list containing these items.
+
+let languagesArray;
+
+languagesArray = ["English", "Urdu", "Arabic", "Turkish"];
+
+console.log(languagesArray[0]);
+
+console.log(languagesArray[1]);
+
+console.log(languagesArray[2]);
+
+console.log(languagesArray[3]);
