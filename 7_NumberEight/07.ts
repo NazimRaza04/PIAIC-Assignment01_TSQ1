8c4a735fa8f05252c106b96889466b10761240e9
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:14:52 +0500

    Add files via upload

---
+++ 7_NumberEight/07.ts
@@ -0,0 +1,23 @@
+// 7. Number Eight: Write addition, subtraction, multiplication, 
+// and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+
+let a = 7;
+let b = 1;
+let c = a + b;
+console.log(c);
+
+let d = 9;
+let e = 1;
+let f = d - e;
+console.log(f);
+
+let g = 2;
+let h = 4;
+let i = g * h;
+console.log(h);
+
+let j = 48;
+let k = 6;
+let l = j / k;
+console.log(l);
