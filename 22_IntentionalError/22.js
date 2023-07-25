0e298acaaa12f16dd2dbd81693ea853d8e0d0ebf
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:33:36 +0500

    Add files via upload

---
+++ 22_IntentionalError/22.js
@@ -0,0 +1,9 @@
+"use strict";
+// 22. Intentional Error: If you haven’t received an array index error in one of your programs
+// yet, try to make one happen. Change an index in one of your programs to produce 
+// an index error. 
+// Make sure you correct the error before closing the program.
+//To print number 4 in array
+let abc = ["1", "2", "3", "4"];
+console.log(abc[4]);
+console.log(abc[3]);
