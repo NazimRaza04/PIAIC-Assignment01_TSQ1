b884ba0ad5c1d9bf47f7795969c97cc577e74332
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:10:49 +0500

    Add files via upload

---
+++ 3_NameCases/03.js
@@ -0,0 +1,8 @@
+"use strict";
+//3. Name Cases: Store a person’s name in a variable
+//and then print that person’s name in lowercase, uppercase, and titlecase.
+let userName = "Eric Newton";
+console.log(userName.toLowerCase());
+console.log(userName.toUpperCase());
+const newName = userName.split(" ").map((l) => l[0].toUpperCase() + l.substr(1)).join(" ");
+console.log(newName);
