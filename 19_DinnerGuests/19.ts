6d8e48e6aefea22e719aae7725019fecb3f60475
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:29:44 +0500

    Add files via upload

---
+++ 19_DinnerGuests/19.ts
@@ -0,0 +1,8 @@
+// 19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, 
+//print a message indicating the number of people you are inviting to dinner.
+
+let dinnerGuests = ["Raheel", "Amjad", "Zubair", "Nomeel"]
+
+dinnerGuests.forEach ((item, index) => console.log (`${item}, You are cordially invited at my place for dinner tonight at 9pm.`));
+
+console.log(`${dinnerGuests.length} people are invited at dinner`);
