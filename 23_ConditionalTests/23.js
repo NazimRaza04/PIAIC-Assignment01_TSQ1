421ef140b3abd8195f14082afa685a8014edcff5
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:34:42 +0500

    Add files via upload

---
+++ 23_ConditionalTests/23.js
@@ -0,0 +1,53 @@
+"use strict";
+// 23. Conditional Tests: Write a series of conditional tests. 
+// Print a statement describing each test and your prediction for the results of each test. 
+// Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates 
+// to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 
+// tests evaluate to False.
+//True:
+//1.
+let boy = 'Imran';
+console.log("Is Imran a boy?");
+console.log(boy == 'Imran');
+//2.
+let car = 'Civic';
+console.log("Is Civic a car?");
+console.log(car == 'Civic');
+//3
+let color = 'Red';
+console.log("Is Red a color?");
+console.log(color == 'Red');
+//4
+let season = 'Winter';
+console.log("Is Winter a season?");
+console.log(season == 'Winter');
+//5
+let device = 'Phone';
+console.log("Is Phone a device?");
+console.log(device == 'Phone');
+//False:
+//1.
+let boy1 = 'Imran';
+console.log("Is Imran a boy?");
+console.log(boy1 == 'imran');
+//2.
+let car1 = 'Civic';
+console.log("Is Civic a car?");
+console.log(car1 == 'civic');
+//3
+let color1 = 'Red';
+console.log("Is Red a color?");
+console.log(color1 == 'red');
+//4
+let season1 = 'Winter';
+console.log("Is Winter a season?");
+console.log(season1 == 'winter');
+//5
+let device1 = 'Phone';
+console.log("Is Phone a device?");
+console.log(device1 == 'phone');
