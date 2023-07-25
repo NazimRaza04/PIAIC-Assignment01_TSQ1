5d8c91fc014abf873a0dedd3ba6159a1ffb1a8c9
Nazim Raza <137108405+NazimRaza04@users.noreply.github.com>
2023-07-25 12:51:21 +0500

    Add files via upload

---
+++ 42_GreatMagicians/42.js
@@ -0,0 +1,16 @@
+"use strict";
+//42. Great Magicians: Start with a copy of your program from Exercise 41. 
+//Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. 
+//Call show_magicians() to see that the list has actually been modified.
+function show_magicians(magicians) {
+    for (let i = 0; i < magicians.length; i++) {
+        console.log(magicians[i].charAt(0).toUpperCase() + magicians[i].slice(1));
+    }
+}
+let magicians = ['Criss Angel', 'David Blaine', 'Apollo Robbins'];
+show_magicians(magicians);
+magicians.forEach((v, i) => magicians[i] = "great " + v);
+function make_great(magicians) {
+    magicians.forEach(v => console.log(v));
+}
+show_magicians(magicians);
